6de20a8b3a1ed4a53ce98a9e8c2f80e3333c7f07
infotechsrealmdarshan <infotechsrealm.darshan@gmail.com>
2026-05-01 10:28:46 +0530

    Add files via upload

--- src/reels/dto/create-reel.dto.ts
+++ src/reels/dto/create-reel.dto.ts
@@ -1,21 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateReelDto {
   @ApiProperty({
     description: 'The URL of the reel video',
     example: 'https://example.com/reel.mp4',
   })
+  @IsString()
+  @IsNotEmpty()
   reelUrl: string;
 
   @ApiProperty({
     description: 'The title of the reel',
     example: 'Amazing Reel',
   })
+  @IsString()
+  @IsNotEmpty()
   title: string;
 
   @ApiProperty({
     description: 'The description of the reel',
     example: 'Check out this cool content!',
   })
+  @IsString()
+  @IsNotEmpty()
   description: string;
 }
