5117eac27e0f8760b6dbe58adbe87337d21ad080
infotechsrealmdarshan <infotechsrealm.darshan@gmail.com>
2026-05-04 13:15:58 +0530

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
   console.log(`Swagger documentation: ${await app.getUrl()}/api`);
 }
